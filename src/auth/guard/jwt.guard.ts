6f0483b31c2b3d7f117e652b739948918fd963d0
Amir <amirkhalid690@gmail.com>
2026-01-14 09:40:04 +0200

    g
    g

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -1,7 +1,6 @@
+import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
-export class JwtGuard extends AuthGuard("jwt"){
-    constructor(){
-        super()
-    }
-}
+@Injectable()
+export class JwtGuard extends AuthGuard("jwt"){}
+  
